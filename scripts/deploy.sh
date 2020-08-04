e3fd6b435331da231a9ccedf4561280699c6f0e3
Nicolas Spencer <nspencer@Nicolass-MacBook-Pro.local>
2020-08-05 02:18:15 +1000

    Changes to deploy.sh. Remove the container before running the new one due to port conflict

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,5 +6,7 @@ echo "***************************************"
 
 #Pulling the image from the registry
 docker pull gcr.io/nicolaspencer/api-server-image
+#Remove the previous container
+docker rm -fv api-container
 #Run the contaniner
 docker run -d -p 5000:5000 --name api-container gcr.io/nicolaspencer/api-server-image
