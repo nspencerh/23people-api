5e8a28ab3a3a82668f73d1db5909fcd092dee931
Nicolas Spencer <nspencer@Nicolass-MacBook-Pro.local>
2020-08-04 20:00:07 +1000

    Add taging the builded image with the GCP registry

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,7 @@ echo "****************************"
 echo "** Building Docker Image ***"
 echo "****************************"
 
+#Build the new image
 docker-compose build --no-cache
+#Tag the builded docker image with the GCP registry
+docker tag api-server-image gcr.io/nicolaspencer/api-server-image
