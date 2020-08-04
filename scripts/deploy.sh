20e14a3538dc9d76a49ad73f4c2e273eac6950c3
Nicolas Spencer <nspencer@Nicolass-MacBook-Pro.local>
2020-08-05 02:28:57 +1000

    Add 6 second sleep to let the container be removed before running the new one

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,5 +8,7 @@ echo "***************************************"
 docker pull gcr.io/nicolaspencer/api-server-image
 #Remove the previous container
 docker rm -fv api-container
-#Run the contaniner
+#Sleep for 6 seconds to give time to remove the old container before running the new one
+sleep 6
+#Run the new contaniner
 docker run -d -p 5000:5000 --name api-container gcr.io/nicolaspencer/api-server-image
