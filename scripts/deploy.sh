d06810a604a31bc65d01d26539e13d6262415402
Nicolas Spencer <nspencer@Nicolass-MacBook-Pro.local>
2020-08-05 19:29:14 +1000

    Force to pull the image from registry instead of using the local one

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,12 @@ echo "***************************************"
 echo "** Deploying image into GCP VM  *******"
 echo "***************************************"
 
-
-echo ">>>> Removing previous image..."
+#Pulling the new builed image to the VM
+echo ">>>> Pulling the new image from the registry..."
+docker pull gcr.io/nicolaspencer/api-server-image:latest
+#Remove the previous container
+echo ">>>> Removing previous contaniner..."
 docker rm -fv api-container
-#Run the new contaniner
+#Run the new contaniner with the latest image from the registry
 echo ">>>> Runing the container....."
-docker run -d -p 5000:5000 --name api-container gcr.io/nicolaspencer/api-server-image
+docker run -d -p 5000:5000 --name api-container gcr.io/nicolaspencer/api-server-image:latest
